be37b2561b6815377a2f97d5e24569db0f983b13
rameshde <densin.ramesh@hs-furtwangen.de>
2019-11-12 11:06:57 +0100

    added my invoiceStatus "OK"

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -6,6 +6,7 @@ sap.ui.define([], function () {
 		statusText: function (sStatus) {
 			var oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
 
+			// Hier werden invoiceStatus geswitcht
 			switch (sStatus) {
 				case "A":
 					return oResourceBundle.getText("invoiceStatusA");
@@ -13,6 +14,8 @@ sap.ui.define([], function () {
 					return oResourceBundle.getText("invoiceStatusB");
 				case "C":
 					return oResourceBundle.getText("invoiceStatusC");
+				case "D":
+					return oResourceBundle.getText("invoiceStatusD");
 				default:
 					return sStatus;
 			}
